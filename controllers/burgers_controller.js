ead267a84e08552d1895d3b26fbbe058246a8cf8
Joshua J. Haller <phone@joshuajhaller.com>
2021-01-13 10:34:05 -0800

    Updated my controllers to match my updated models.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,11 @@
-let express = require("express");
+const express = require("express");
+const burger = require("../models/burger.js");
 
 const router = express.Router();
 
-let burger = require("../models/burger.js");
-
-router.get("/", function (req, res) {
-  burger.all(function (data) {
-    let hbsObject = {
+router.get("/", (req, res) => {
+  burger.selectBurgers((data) => {
+    const hbsObject = {
       burgers: data,
     };
     console.log(hbsObject);
@@ -14,35 +13,43 @@ router.get("/", function (req, res) {
   });
 });
 
-router.post("/api/burgers", function (req, res) {
-  burger.create(
+router.post("/api/burgers", (req, res) => {
+  burger.insertBurger(
     ["burger_name", "devoured"],
     [req.body.name, req.body.devoured],
-    function (result) {
-      res.json({ id: result.insertId });
+    (result) => {
+      res.json({ id: result.insertID });
+      console.log(result);
     }
   );
 });
 
-router.put("/api/burgers/:id", function (req, res) {
-  let condition = "id = " + req.params.id;
+router.put("/api/burgers/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("id", id);
+  console.log(req.body);
+  console.log(req.body.devoured);
 
-  console.log("condition", condition);
-
-  burger.update(
-    {
-      devoured: req.body.devoured,
-    },
-    condition,
-    function (result) {
-      if (result.changedRows === 0) {
-        return res.status(404).end();
-      }
+  burger.updateBurger([req.body.devoured], id, (result) => {
+    if (result.changeRows == 0) {
+      return res.status(404).end();
+    } else {
       res.status(200).end();
     }
-  );
+  });
+});
+
+router.delete("/api/burgers/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("id", id);
+
+  burger.deleteBurger(id, (result) => {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
 });
 
 module.exports = router;
-
-//es6 this shit!
