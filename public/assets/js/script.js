7eb5d53328ed9c68c3640a68784d64f5b607fbda
Joshua J. Haller <phone@joshuajhaller.com>
2021-01-12 20:18:05 -0800

    added a script.js file.

---
+++ public/assets/js/script.js
@@ -0,0 +1,41 @@
+const { post } = require("../../../controllers/burgers_controller");
+
+$(function () {
+  $(".devour-button").on("click", function (event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newDevoured");
+
+    var newEatenBurger = {
+      devoured: newDevoured,
+    };
+
+    // console.log(newDevoured);
+    // PUT request here!
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenBurger,
+    }).then(function () {
+      console.log("Chaned devoured state to:", newDevoured);
+      location.reload();
+    });
+  });
+});
+
+$(function () {
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+
+    let newBurger = {
+      name: $("#ca").val().trim(),
+      eaten: $("[name=eaten]:checked").val().trim(),
+    };
+
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("Created a new burger!");
+      location.reload();
+    });
+  });
+});
